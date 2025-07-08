f8e01aff4eec1b457b2694d26caa48cfffc3b4a3
AndresMac318 <andres98belt@gmail.com>
2025-07-08 18:46:16 -0500

    first commit: base structure

---
+++ hotels_apirest/src/main/java/com/cheil/hotels_apirest/HotelsApirestApplication.java
@@ -0,0 +1,13 @@
+package com.cheil.hotels_apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelsApirestApplication.class, args);
+	}
+
+}
---
+++ hotels_apirest/src/test/java/com/cheil/hotels_apirest/HotelsApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheil.hotels_apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelsApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
